01e4c59ed792c6f2c906581c56896e1c3e2bd542
Mr-Genta <andreanardinta2@gmail.com>
2024-03-11 21:30:47 +0700

    pertama

---
+++ src/weak1/latihan1.java
@@ -0,0 +1,16 @@
+package weak1;
+
+public class latihan1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Halo,nama saya adalah Genta");
+		
+		System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+		
+		System.out.println("Saya sedang menulis program Java");
+
+	}
+
+}
---
+++ src/weak1/latihan2.java
@@ -0,0 +1,33 @@
+package weak1;
+
+public class latihan2 
+{
+	public static void main(String[] args) 
+	{
+		int d = 10;
+		double phi = 3.14;
+		double r,K;
+		r = d / 2;
+		K = 2 * phi * r;
+		
+		System.out.println("keliling Lingkaran = "+ K);
+		
+	
+		double L,a,b;
+		a = 6;
+		b = 8;
+		L = 0.5 * a * b;
+		
+		System.out.println("Luas segitiga siku-siku ="+ L);
+		
+		
+		double V,t,D,R;
+		D = 5;
+		t = 10;
+		R = D / 2;
+		V = phi * R * R * t;
+		
+		System.out.println("Volume tabung ="+ V);
+		
+	}
+}
